77a69ddf94d08b8ba55fe7a073a786def8a65a39
paul <paul@gigico.tv>
2024-10-24 10:51:55 +0800

    initial project

---
+++ src/test/java/com/jiawa/demo/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiawa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
