0beab094e93bbbd706af8da4d15c6e74279c5b14
paul <paul@gigico.tv>
2024-10-27 18:57:50 +0800

    实现一个简单的注册接口

--- train-member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ train-member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.jiawa.train.member.controller;
 import com.jiawa.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,9 @@ public class MemberController {
     @GetMapping("/count")
     public Integer count() {
         return memberService.count();
+    }
+    @PostMapping("/register")
+    public long register(String mobile) {
+        return memberService.register(mobile);
     }
 }
